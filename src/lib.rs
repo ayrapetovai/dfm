1df1e69bef919a2ab2832a048cef6f2e8f327e60
ayrapetovai <ayeaye.post@gmail.com>
2024-06-02 23:07:20 +0300

    absolut path calculation refactored

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@ pub fn file_path_relative_to(file_abs_path: &PathBuf, relative_to_abs_path: &Pat
     let mut path_components = Vec::new();
     for target_file_parent in file_abs_path.ancestors() {
         if relative_to_abs_path.eq(target_file_parent) {
-            target_file_rel_to_target_dir_path_opt = Some(PathBuf::from_iter(path_components));
+            target_file_rel_to_target_dir_path_opt = Some(PathBuf::from_iter(&path_components));
             break;
         }
         if let Some(filename) = target_file_parent.file_name() {
@@ -32,36 +32,87 @@ pub fn file_path_relative_to(file_abs_path: &PathBuf, relative_to_abs_path: &Pat
         }
     }
 
-    // TODO if file does not belong to the given path, return
-    //  something like "./../../../home/user/other/target/dir/file"
-    //  when this relative path will be concatenated with the source directory path we'll get:
-    //  /home/user/dotfiles/./../../../home/user/other/target/dir/file
-    //  which will be resolved to /home/user/other/target/dir/file
-    //  and added as /home/user/dotfiles/root_home/user/other/target/dir/file
-    target_file_rel_to_target_dir_path_opt.unwrap()
+    if target_file_rel_to_target_dir_path_opt.is_some() {
+        target_file_rel_to_target_dir_path_opt.unwrap()
+    } else {
+        let mut target_file_rel_to_target_dir_path_with_backs = String::from(file_abs_path.to_str().unwrap());
+        for _ in 0..path_components.len() {
+            target_file_rel_to_target_dir_path_with_backs.insert_str(0, "/..")
+        }
+        target_file_rel_to_target_dir_path_with_backs.insert_str(0, ".");
+        PathBuf::from(target_file_rel_to_target_dir_path_with_backs)
+    }
 }
 
 pub fn filepath_in_source_dir(config: &Config, target_dir_abs_path: &PathBuf, source_dir_abs_path: &PathBuf, target_abs_path: &PathBuf, add_postfix_opt: Option<&str>) -> PathBuf {
-    let regexp_for_leading_dot_in_filename = Regex::new(r"^\.").unwrap();
-    let regexp_for_leading_dot_in_path = Regex::new(r"/\.").unwrap();
+    let regexp_for_leading_dot_in_filename = Regex::new(r#"^\."#).unwrap();
+    let regexp_for_leading_dot_in_path = Regex::new(r#"/\.[^.]"#).unwrap();
 
     let dot_prefix = config.dot_prefix.clone().unwrap();
     let slash_dot_prefix = String::from_iter(vec!["/", &dot_prefix]);
 
-    let target_file_rel_to_target_dir_path = file_path_relative_to(&target_abs_path, &target_dir_abs_path);
+    let target_file_rel_to_target_dir_path = file_path_relative_to(target_abs_path, &target_dir_abs_path);
 
     println!("target file path relative to target directory {:?}", target_file_rel_to_target_dir_path);
 
     // replace dots in filenames and dirnames to dot_prefix from config
-    let source_file_rel_to_source_dir_path = regexp_for_leading_dot_in_filename
-        .replace(target_file_rel_to_target_dir_path.to_str().unwrap(), &dot_prefix).to_string();
-    let mut source_file_rel_to_source_dir_path = regexp_for_leading_dot_in_path
-        .replace_all(&source_file_rel_to_source_dir_path, &slash_dot_prefix).to_string();
+    let filename = regexp_for_leading_dot_in_filename.replace(target_file_rel_to_target_dir_path.file_name().unwrap().to_str().unwrap(), &dot_prefix).to_string();
+    let parent = regexp_for_leading_dot_in_filename.replace(target_file_rel_to_target_dir_path.parent().unwrap().to_str().unwrap(), &dot_prefix).to_string();
+    let mut dirname = regexp_for_leading_dot_in_path.replace_all(&parent, &slash_dot_prefix).to_string();
+    if !dirname.is_empty() {
+        dirname.push('/');
+    }
+    else {
+        dirname.push_str("./");
+    }
 
+    let mut source_file_rel_to_source_dir_path = String::from_iter(vec![dirname, filename]);
     if let Some(postfix) = add_postfix_opt {
         source_file_rel_to_source_dir_path = source_file_rel_to_source_dir_path.add(postfix);
     }
 
     println!("source file path relative to source directory {}", source_file_rel_to_source_dir_path);
-    return PathBuf::from_iter(vec![source_dir_abs_path.to_str().unwrap(), &source_file_rel_to_source_dir_path]);
+    let ret = PathBuf::from_iter(vec![source_dir_abs_path.to_str().unwrap(), &source_file_rel_to_source_dir_path]);
+    return remove_dots_from_path(&ret);
+}
+
+fn remove_dots_from_path(path: &PathBuf) -> PathBuf {
+    if path.to_str().unwrap() == "/" {
+        return PathBuf::from(path);
+    }
+ 
+    let mut go_back_counter = 0;
+    let mut ret = String::new();
+    for ancestor in path.iter().rev() {
+        let name= ancestor.to_str().unwrap();
+        if name == ".." {
+            go_back_counter += 1;
+        } else if name != "." && name != "/" {
+            if go_back_counter > 0 {
+                go_back_counter -=1;
+            } else if !name.is_empty() {
+                ret.insert_str(0, name);
+                ret.insert(0, '/');
+            }
+        }
+    }
+    if go_back_counter > 0 {
+        ret.remove(0);
+    }
+    for _ in 0..go_back_counter {
+        ret.insert_str(0, "../");
+    }
+    return PathBuf::from(ret);
+}
+
+#[test]
+fn test_remove_dots_from_path() {
+    assert_eq!(remove_dots_from_path(&PathBuf::from("/")), PathBuf::from("/"));
+    assert_eq!(remove_dots_from_path(&PathBuf::from("/a")), PathBuf::from("/a"));
+    assert_eq!(remove_dots_from_path(&PathBuf::from("/a/e")), PathBuf::from("/a/e"));
+    assert_eq!(remove_dots_from_path(&PathBuf::from("/a/b/e/..")), PathBuf::from("/a/b"));
+    assert_eq!(remove_dots_from_path(&PathBuf::from("/a/b/c/../../d/e")), PathBuf::from("/a/d/e"));
+    assert_eq!(remove_dots_from_path(&PathBuf::from("/a/b/../../d/e")), PathBuf::from("/d/e"));
+    assert_eq!(remove_dots_from_path(&PathBuf::from("/a/../../d/e")), PathBuf::from("../d/e"));
+    assert_eq!(remove_dots_from_path(&PathBuf::from("/../../d/e")), PathBuf::from("../../d/e"));
 }
--- src/main.rs
+++ src/main.rs
@@ -114,6 +114,10 @@ enum Command {
         /// Move target file to the source directory, and create a symlink in the target directory.
         #[arg(long, short, num_args = 0, default_value_t = false)]
         symlink: bool,
+        
+        /// Run only checks, no changes will be made to filesystem.
+        #[arg(long, short, num_args = 0, default_value_t = false)]
+        dry_run: bool,
     },
 
     // must check conflicts
@@ -163,6 +167,7 @@ fn add_command(config: &Config, args: &Args) {
         allow_foreign: foreign,
         overwrite,
         symlink,
+        dry_run,
         ..
     } = &args.command else {
         panic!("unreachable code reached: command {:?} is not `add`", args.command)
@@ -295,31 +300,28 @@ fn add_command(config: &Config, args: &Args) {
             };
             println!("target symlink {:?} points to {:?}", target_symlink_abs_path, target_symlink_pointee_abs_path);
 
-            let source_symlink_file_abs_path : PathBuf = filepath_in_source_dir(&config, &target_dir_abs_path, &source_dir_abs_path, &target_symlink_abs_path, Some(".symlink"));
-            let source_symlink_file_points_to_right_target = if source_symlink_file_abs_path.exists() {
-                match fs::read_to_string(&source_symlink_file_abs_path) {
+            let source_symlink_file_abs_path = filepath_in_source_dir(&config, &target_dir_abs_path, &source_dir_abs_path, &target_symlink_abs_path, Some(".symlink"));
+            if source_symlink_file_abs_path.exists() {
+                let source_symlink_file_points_to_right_target = match fs::read_to_string(&source_symlink_file_abs_path) {
                     Ok(file_content) => {
-                        println!("source symlink file {:?} points to {}", source_symlink_file_abs_path, file_content);
+                        println!("source symlink file {:?} points to \"{}\"", source_symlink_file_abs_path, file_content);
                         file_content.trim().eq(target_symlink_pointee_rel_path.to_str().unwrap())
                     },
                     _ => false
-                }
-            } else {
-                false
-            };
-
-            if *overwrite || !source_symlink_file_points_to_right_target {
-                if !source_symlink_file_points_to_right_target {
-                    print!("source symlink file points to the wrong file");
-                }
-                target_to_source_list.push(AddTask::CreateSymlinkFilePointer(source_symlink_file_abs_path, target_symlink_pointee_rel_path.to_str().unwrap().to_owned()));
-            } else if source_symlink_file_points_to_right_target {
-                println!("for target symlink {:?}, source symlink file {:?} already exists, skipping...", target_symlink_abs_path, source_symlink_file_abs_path);
-            } else if !target_symlink_pointee_abs_path.starts_with(&source_dir_abs_path) {
-                println!("for target symlink {:?}, does not have a source symlink file {:?}", target_symlink_abs_path, source_symlink_file_abs_path);
-                target_to_source_list.push(AddTask::CreateSymlinkFilePointer(source_symlink_file_abs_path, target_symlink_pointee_rel_path.to_str().unwrap().to_owned()));
-            } else {
-                println!("target symlink {:?} pointee is managed as {:?}, to add a symlink to source directory use --overwrite", source_symlink_file_abs_path, target_symlink_pointee_abs_path);
+                };
+                if *overwrite || !source_symlink_file_points_to_right_target {
+                    if !source_symlink_file_points_to_right_target {
+                        println!("source symlink file points to the wrong file, must be {:?}", &target_symlink_pointee_rel_path);
+                    }
+                    target_to_source_list.push(AddTask::CreateSymlinkFilePointer(source_symlink_file_abs_path.clone(), target_symlink_pointee_rel_path.to_str().unwrap().to_owned()));
+                } else if source_symlink_file_points_to_right_target {
+                    println!("for target symlink {:?}, source symlink file {:?} already exists, skipping...", target_symlink_abs_path, source_symlink_file_abs_path);
+                } else if !target_symlink_pointee_abs_path.starts_with(&source_dir_abs_path) {
+                    println!("for target symlink {:?}, does not have a source symlink file {:?}", target_symlink_abs_path, source_symlink_file_abs_path);
+                    target_to_source_list.push(AddTask::CreateSymlinkFilePointer(source_symlink_file_abs_path.clone(), target_symlink_pointee_rel_path.to_str().unwrap().to_owned()));
+                } else {
+                    println!("target symlink {:?} pointee is managed as {:?}, to add a symlink to source directory use --overwrite", source_symlink_file_abs_path, target_symlink_pointee_abs_path);
+                };
             };
             target_symlink_pointee_abs_path
         } else {
@@ -351,8 +353,7 @@ fn add_command(config: &Config, args: &Args) {
         }
 
         if !target_abs_path.starts_with(&target_dir_abs_path) {
-            println!("target {:?} does not reside in target directory {:?}", target_abs_path, target_dir_abs_path);
-            println!("the adoption of foreign files is not implemented yet");
+            println!("target {:?} does not reside in target directory {:?}, skipping...", target_abs_path, target_dir_abs_path);
             continue;
         }
 
@@ -435,7 +436,7 @@ fn add_command(config: &Config, args: &Args) {
 
             eprintln!("no conflict detected for target {:?}", target_abs_path);
         } else if true { // TODO if verbose
-            println!("source file {:?} does not exist", source_file_abs_path)
+            println!("source file {:?} does not exist", source_file_abs_path);
         }
 
         target_to_source_list.push(AddTask::Copy(target_abs_path, source_file_abs_path));
@@ -446,6 +447,10 @@ fn add_command(config: &Config, args: &Args) {
     // TODO filter target duplicates
     // TODO file conflicts like: (tgt1 -> src1) and (tgt2 -> src1) and (tgt1 != tgt2)
 
+    if *dry_run {
+        println!("dry run specified, no changes will be made");
+    }
+
     if target_to_source_list.is_empty() {
         println!("nothing to do");
         return; // TODO with success
@@ -454,9 +459,15 @@ fn add_command(config: &Config, args: &Args) {
     println!("::copy procedure begins, {} tasks", target_to_source_list.len());
 
     for add_task in target_to_source_list {
+        if *dry_run {
+            println!("dry run: copy {:?}", add_task);
+            continue;
+        } else {
+            println!("copy {:?}", add_task);
+        }
+
         match add_task {
             AddTask::Copy(target_file, source_file) => {
-                println!("copying {:?} to {:?}", target_file, source_file);
                 if let Err(e) = fs::remove_file(source_file.clone()) {
                     println!("failed to remove source {:?}: {}", source_file, e);
                 } else {
