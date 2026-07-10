3a0c591ee2f151c40cf9dbfbe17323cf14a274b5
ayrapetovai <ayeaye.post@gmail.com>
2026-07-11 01:02:59 +0300

    test launcher upgraded

--- tests/launcher.sh
+++ tests/launcher.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/bash
 
-set -eux
+set -euP
+
+PROGRAMM_NAME_IN_SHELL="dfm"
+EXECUTABLE_NAME="dfm"
 
 TESTS_DIR=`readlink -f "$(dirname "$0")"`
-
 PROJECT_ROOT=$(readlink -f "$TESTS_DIR/../")
-# echo "PROJECT_ROOT=$PROJECT_ROOT"
-# echo "TESTS_DIR=$TESTS_DIR"
+DEBUG_EXECUTABLE="$PROJECT_ROOT/target/debug/$EXECUTABLE_NAME"
+RELEASE_EXECUTABLE="$PROJECT_ROOT/target/release/$EXECUTABLE_NAME"
 
-DEBUG_EXECUTABLE="$PROJECT_ROOT/target/debug/dfm"
-RELEASE_EXECUTABLE="$PROJECT_ROOT/target/release/dfm"
-
-EXECUTABLE=""
+EXECUTABLE=
 if [[ -f "$DEBUG_EXECUTABLE" ]]; then
     EXECUTABLE="$DEBUG_EXECUTABLE"
 elif [[ -f "$RELEASE_EXECUTABLE" ]]; then
@@ -23,26 +22,35 @@ fi
 
 export EXECUTABLE
 
-function dfm() {
-    "$EXECUTABLE" "$@"
-}
+eval 'function '$PROGRAMM_NAME_IN_SHELL'() { "$EXECUTABLE" "$@"; }'
+export "$PROGRAMM_NAME_IN_SHELL"
 
-export dfm
+readonly TMP_HOME=$(mktemp -d)
+trap 'rm -rf -- "$TMP_HOME"' EXIT
 
-TMP_HOME=$(mktemp -d)
 export HOME="$TMP_HOME"
 cd $HOME
 
-TEST_CASES=$(find $TESTS_DIR -name 'test*.sh')
+TEST_CASES=$(find "$TESTS_DIR" -type f -name 'test*.sh')
 
-echo "running tests"
+echo "running $(echo $TEST_CASES | wc -l) tests"
+
+SUCCED_COUNTER=0
+FAILED_COUNTER=0
 
 for test_case in "$TEST_CASES" ; do
     test_name="$(basename $test_case)"
-    if ( source "$test_case" ) ; then
+    if ( set -eEux; source "$test_case" ) ; then
         echo "---- $test_name ✅"
+        SUCCED_COUNTER=$((SUCCED_COUNTER + 1))
     else
         echo "---- $test_name ❌"
+        FAILED_COUNTER=$((FAILED_COUNTER + 1))
     fi
 done
 
+echo "succed $SUCCED_COUNTER"
+echo "failed $FAILED_COUNTER"
+
+test $FAILED_COUNTER -eq 0
+exit $?
