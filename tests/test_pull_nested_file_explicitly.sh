e3ff5993ac69b923c9b143cff28fe4de99cffe57
ayrapetovai <ayeaye.post@gmail.com>
2026-07-26 02:47:27 +0300

    test pull of nested files

---
+++ tests/test_pull_nested_file_explicitly.sh
@@ -0,0 +1,17 @@
+CONTENT="$(uuid)"
+dfm init dotfile
+mkdir -p "$PWD/dir1/dir2"
+write "$CONTENT" "$PWD/dir1/dir2/file.txt"
+dfm add dir1
+
+assert_source "dir1/dir2/file.txt"
+rm -rf dir1
+
+dfm pull dir1
+assert -f "$PWD/dir1/dir2/file.txt"
+assert_content_eq "$PWD/dir1/dir2/file.txt" "$CONTENT"
+
+rm -rf dir1
+dfm pull dir1/dir2
+assert -f "$PWD/dir1/dir2/file.txt"
+assert_content_eq "$PWD/dir1/dir2/file.txt" "$CONTENT"
