5b5d33592ab3e62aea7beb6c854718014ebf1927
ayrapetovai <ayeaye.post@gmail.com>
2026-07-27 04:20:44 +0300

    test for ignore several directories

---
+++ tests/test_ignore_several_directories.sh
@@ -0,0 +1,12 @@
+dmf init dotfiles
+mkdir -p dir1/a
+mkdir -p dir2/b
+mkdir -p dir3/b
+mkdir not_ignored_dir
+write "text" file.txt
+
+dfm ignore dir1
+dfm ignore dir2 dir3
+
+! dmf status --all | grep -q 'dir1|dir2|dir3'
+
