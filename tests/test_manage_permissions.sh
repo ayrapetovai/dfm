71c32493ff6a2b1999fb2ad63ef608978760d1ad
ayrapetovai <ayeaye.post@gmail.com>
2026-07-29 04:09:25 +0300

    test with chmod fixed

--- tests/test_manage_permissions.sh
+++ tests/test_manage_permissions.sh
@@ -1,9 +1,9 @@
 dfm init dotfiles
 touch file.txt
-chmod a+x file.txt
-assert "775" = "$(stat -c '%a' file.txt)"
+chmod 0755 file.txt
+assert "755" = "$(stat -c '%a' file.txt)"
 dfm add file.txt
-assert "775" = "$(stat -c '%a' $PWD/dotfiles/file.txt)"
+assert "755" = "$(stat -c '%a' $PWD/dotfiles/file.txt)"
 rm file.txt
 dfm pull file.txt
-assert "775" = "$(stat -c '%a' file.txt)"
+assert "755" = "$(stat -c '%a' file.txt)"
