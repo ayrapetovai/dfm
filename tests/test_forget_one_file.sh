4a1d1f61fedc2838d77d3fc547a868be37861063
ayrapetovai <ayeaye.post@gmail.com>
2026-07-31 00:12:47 +0300

    fix assertion

--- tests/test_forget_one_file.sh
+++ tests/test_forget_one_file.sh
@@ -2,15 +2,15 @@ dfm init dotfiles
 write "text1" file1.txt
 write "text2" file2.txt
 mkdir d
-write "text4" d/file3.txt
+write "text3" d/file3.txt
 
 dfm add .
 dfm forget file2.txt
 
 assert_source "file1.txt"
-assert_source "d/file4.txt"
+assert_source "d/file3.txt"
 
-cat ./.local/state/dfm/state.toml | grep -q "file1.txt"
-cat ./.local/state/dfm/state.toml | grep -q "d/file3.txt"
-! cat ./.local/state/dfm/state.toml | grep -q "file2.txt"
+grep -q "file1.txt" ./.local/state/dfm/state.toml
+grep -q "d/file3.txt" ./.local/state/dfm/state.toml
+assert_fail grep -q "file2.txt" ./.local/state/dfm/state.toml
 
